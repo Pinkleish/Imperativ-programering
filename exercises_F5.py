bf807c718d2c1ef5f36b11fd349a7b2630b428d9
Pinkleish <axel.dahl@hotmail.se>
2025-09-16 18:00:56 +0200

    Nästan färdig med exercise 5

--- exercises_F5.py
+++ exercises_F5.py
@@ -1,5 +1,5 @@
 # These exercises relate to Föreläsning 5 - Lists, For Loops, Sorting
-
+import random
 
 print('==== Q1. ====')
 # This code creates an array from string literals:
@@ -88,6 +88,7 @@ def dice_addition(array):
         print(dice_sum)
 
 dice_addition(dice)
+# ***I didn't realise sum was a command in python until later in this assignment.***
 
 # Having tested your code, convert it into a function which takes an array as an argument.
 # Invoke that function on the original dice array and print the result.
@@ -122,39 +123,74 @@ print('==== Q7. ====')
 # Now we practice calling functions within functions and loops.
 
 # Define a function is_even, to computer whether an integer is even.
+def is_even(integer):
+    if integer % 2 == 0:
+        print(str(integer) + " is even")
+    else:
+        print(str(integer) + " is odd")
+is_even(5)
 # Define another function, which sums the sides of a dice-array, but only sum the sides which are even.
+def sum_all_even(sides):
+    dice_array = create_dice_array(sides)
+    tot = 0
+    for side in dice_array:
+        if side % 2 == 0:
+            tot = tot + side
+    return tot
 
-exit()
+print(sum_all_even(6))
+#exit()
 print('==== Q8. ====')
 
 # Define a function which "rolls" the dice -- i.e. returns one of its elements at random, with equal probability.
 # (Hint: adapt the 'guessing' code from the F3 script).
 
 # Use a for loop to roll the dice 10 times, printing out the result each time, to verify it is working correctly.
+def dice_roll(sides):
+    dice_array = create_dice_array(sides)
+    for x in range(1,11):
+        roll = random.randint(1, len(dice_array))
+        print(roll)
 
-exit()
+dice_roll(6)
+
+#exit()
 print('==== Q9. ====')
 
 # Now we want to count the occurrences of rolls, i.e. how many times a 1 was rolled, how many times a 2 was rolled, etc.
 # In Python, we can create an array of zero's like this: [0] * 5
 # Create an array of zeros with a length equal to the number of sides of the 'dice' variable. Use an expression for this length.
+zero_array = [0] * len(dice)
+print(zero_array)
 # Use a for loop to roll the dice 10 times, this time incrementing the element of the count array.
 # For example, if we roll a 3, we increment the third element of the array. (Think: which index is this?)
+for x in range(1,11):
+    roll = random.randint(1,6)
+    zero_array[roll-1] += 1
+print(zero_array)
 
 # Use this variable.
-# roll_counts =
-
-exit()
+roll_counts = sum(zero_array)
+print(roll_counts)
+#exit()
 print('==== Q10. ====')
 
 # Now we try to visualize the results.
 # Write a function containing a for loop to compute the maximum value in an array.
+maximum = 0
+for number in zero_array:
+    if number > maximum:
+        maximum = number
+print(maximum)
 # Create a new array of integers based on the counts, scaling all the values linearly so that the maximum becomes 40.
+scaled_results = []
+proportions = 40/maximum
+for number in zero_array:
+    scaled_results = scaled_results + [number*proportions]
+print(scaled_results)
 
 # i.e. if the max count is 100, all counts are scaled by a factor of 0.4
-
 # Store it in the variable scaled_results:
-# scaled_results =
 
 # Invoke this function to visualize the results:
 def visualize_counts(viz_dice_faces, viz_roll_counts, viz_scaled_counts):
@@ -163,7 +199,6 @@ def visualize_counts(viz_dice_faces, viz_roll_counts, viz_scaled_counts):
         viz_roll_count = viz_roll_counts[i]
         viz_scaled_count = viz_scaled_counts[i]
         print(f"{face:>4} {viz_roll_count:>8} {'*' * viz_scaled_count}")
-
 exit()
 print('==== Q11. ====')
 
