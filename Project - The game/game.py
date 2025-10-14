a8d3c6eb933aa018dd0a8c39a6ff2f42e544c1a1
Pinkleish <axel.dahl@hotmail.se>
2025-10-14 15:43:40 +0200

    Added entrance function

--- Project - The game/game.py
+++ Project - The game/game.py
@@ -1,3 +1,4 @@
+import random
 def dungeon_size_input():
     print(f"Select a height:")
     height_raw = int(input())
@@ -36,6 +37,33 @@ def dungeon_frame(array):
     for i in range(1,height-1):
         for j in range(1,length-1):
                 array[i][j] = " "
-    for row in array:
-        print("".join(row))
+
 dungeon_frame(dungeon_map)
+
+def dungeon_entrance(array):
+    #1. Select a random point on the map
+    #while both are = 0 select new points
+    #if nor the x or y is on the frame:
+    #randomly pick an axis at set it to 0
+    #set that coordinate to = " "
+    height_random = random.randint(0,height-1)
+    length_random = random.randint(0,length-1)
+    while height_random == 0 and length_random == 0:
+        height_random = random.randint(0,height-1)
+        length_random = random.randint(0,length-1)
+    if 0 < height_random < height-1 and 0 < length_random < length-1:
+        coinflip = random.randint(1,2)
+        if coinflip % 2 == 0:
+            length_random = 0
+        else:
+            height_random = 0
+    array[height_random][length_random] = " "
+
+
+
+
+dungeon_entrance(dungeon_map)
+
+#Prints the map
+for row in dungeon_map:
+    print("".join(row))
