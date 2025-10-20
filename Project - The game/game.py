2afffaa7c316f27141baf1c971df9f49df5d757b
Pinkleish <axel.dahl@hotmail.se>
2025-10-20 10:18:05 +0200

    Added treasure, traps & started inner walls

--- Project - The game/game.py
+++ Project - The game/game.py
@@ -1,25 +1,53 @@
 import random
+TREASURE_ICON = "S"
+TRAP_ICON = "F"
+
 def dungeon_size_input():
+    # Asks user to enter a number
     print(f"Select a height:")
     height_raw = int(input())
+    # Makes sure that the number isn't too small
     while height_raw < 5:
         print(f"Selected height too small")
         height_raw = int(input())
-    height_local = height_raw
-    print(f"Height has been set to {height_local}")
+    # Reaffirms the selected height
+    print(f"Height has been set to {height_raw}")
 
     print(f"Select a length:")
     length_raw = int(input())
     while length_raw < 5:
         print(f"Selected length is too small")
         length_raw = int(input())
-    length_local = length_raw
-    print(f"Length has been set to {length_local}")
-    return height_local, length_local
+    print(f"Length has been set to {length_raw}")
+    # Returns the values and then defines global variables as the return of the function once called
+    return height_raw, length_raw
 height, length = dungeon_size_input()
+
+def dungeon_attributes():
+    # Asks user to give 3 numbers for different attributes
+    print(f"You now need to select the amount of each attribute for this dungeon")
+    print(f"Select amount of treasure:")
+    treasure_raw = int(input())
+    print(f"Select amount of traps:")
+    traps_raw = int(input())
+    print(f"Select amount of inner walls:")
+    walls_raw = int(input())
+    # Makes sure that the sum of the attributes isn't bigger than the area inside of the dungeon walls
+    while (treasure_raw + traps_raw + walls_raw) > ((height-2) * (length-2)):
+        print(f"The total amount of attributes is larger than the dungeons area, please select again:")
+        treasure_raw = int(input())
+        traps_raw = int(input())
+        walls_raw = int(input())
+    print(f"Amount of treasure is set to: {treasure_raw}")
+    print(f"Amount of traps is set to: {traps_raw}")
+    print(f"Amount of walls is set to: {walls_raw}")
+    return treasure_raw, traps_raw, walls_raw
+treasure, traps, walls = dungeon_attributes()
+
 
 dungeon_map = []
 def dungeon_size(array):
+    # Creates an empty 2d array based on height and length
     for i in range(height):
         array.append([])
         for j in range(length):
@@ -28,12 +56,14 @@ def dungeon_size(array):
 dungeon_size(dungeon_map)
 
 def dungeon_filled(array):
+    # Fills the empty array with "#"
     for i in range(len(array)):
         for j in range(len(array[i])):
             array[i][j] = "#"
 dungeon_filled(dungeon_map)
 
 def dungeon_frame(array):
+    # Fills out everything but the outer walls with " "
     for i in range(1,height-1):
         for j in range(1,length-1):
                 array[i][j] = " "
@@ -41,28 +71,77 @@ def dungeon_frame(array):
 dungeon_frame(dungeon_map)
 
 def dungeon_entrance(array):
-    #1. Select a random point on the map
-    #while both are = 0 select new points
-    #if nor the x or y is on the frame:
-    #randomly pick an axis at set it to 0
-    #set that coordinate to = " "
+    # Select a random point inside of the walls
     height_random = random.randint(0,height-1)
     length_random = random.randint(0,length-1)
+    # While both are = 0 select new points, this makes sure it isn't a corner
     while height_random == 0 and length_random == 0:
         height_random = random.randint(0,height-1)
         length_random = random.randint(0,length-1)
+     # If no point is on the frame then randomly set either height or length to = 0
     if 0 < height_random < height-1 and 0 < length_random < length-1:
         coinflip = random.randint(1,2)
         if coinflip % 2 == 0:
             length_random = 0
         else:
             height_random = 0
+    # Set the resulting coordinate to = " "
     array[height_random][length_random] = " "
 
-
-
-
 dungeon_entrance(dungeon_map)
+
+def dungeon_inner_walls(array):
+    walls_placed = 0
+    while walls_placed != walls:
+        height_random = random.randint(2,height-3)
+        length_random = random.randint(2,length-3)
+        # Randomly assign if the randomly placed wall should expand backwards or forwards and then assign horizontal/vertical
+        wall_expand_random = random.randint(-1,1)
+        wall_direction_random = random.randint(1,2)
+        wall_expand_times_random = random.randint()
+        if wall_direction_random % 2 == 0:
+
+
+
+def dungeon_treasure(array):
+    # Initiate a counter to keep track of how many treasures have been placed
+    treasure_placed = 0
+    while treasure_placed != treasure:
+        # Pick random coordinate inside of the frame, -2 to make sure that the check always is inside of my array
+        height_random = random.randint(1,height-2)
+        length_random = random.randint(1,length-2)
+        # Check if there is already a treasure in any adjacent coordinate 2+ since its an exclusive upper bound
+        adjacent_empty = 0
+        if array[height_random][length_random] == " ":
+            for i in range(height_random-1,height_random+2):
+                for j in range(length_random-1,length_random+2):
+                    if array[i][j] != TREASURE_ICON:
+                        adjacent_empty += 1
+        # If the checked grid is empty then place a treasure
+        if adjacent_empty == 9:
+            array[height_random][length_random] = TREASURE_ICON
+            treasure_placed += 1
+
+dungeon_treasure(dungeon_map)
+
+def dungeon_traps(array):
+    traps_placed = 0
+    while traps_placed != traps:
+        height_random = random.randint(1,height-2)
+        length_random = random.randint(1,length-2)
+        adjacent_empty = 0
+        # Makes sure the selected coordinate isn't a treasure
+        if array[height_random][length_random] == " ":
+            for i in range(height_random-1,height_random+2):
+                for j in range(length_random-1,length_random+2):
+                    if array[i][j] != TRAP_ICON:
+                        adjacent_empty += 1
+        if adjacent_empty == 9:
+            array[height_random][length_random] = TRAP_ICON
+            traps_placed += 1
+
+dungeon_traps(dungeon_map)
+
 
 #Prints the map
 for row in dungeon_map:
